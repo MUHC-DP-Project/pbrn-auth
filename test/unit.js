f9b2347371bc1ea9366890877ff549a8a194cc2c
Nathan <nathanael.lemma@mail.mcgill.ca>
2020-11-20 01:27:16 -0500

    initial test setup for muhc-auth

---
+++ test/unit.js
@@ -0,0 +1,8 @@
+const assert = require('chai').assert;
+//const app = require('../src/index');
+
+describe('App',function(){
+    it('first test', function(){
+        assert.equal(true, true);
+    });
+});
